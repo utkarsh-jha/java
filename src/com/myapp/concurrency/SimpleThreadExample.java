a9e755fed06998b81fd4cd0dae128d441a2c4b14
Utkarsh Jha <utkarshj@cdac.in>
2020-07-30 17:02:00 +0530

    initial push

---
+++ src/com/myapp/concurrency/SimpleThreadExample.java
@@ -0,0 +1,24 @@
+package com.myapp.concurrency;
+
+class ThreadLogic extends Thread{
+
+    @Override
+    public void run() {
+         
+    }
+    
+    
+    
+}
+
+public class SimpleThreadExample {
+    /*
+        create a list os string
+            
+    */
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
